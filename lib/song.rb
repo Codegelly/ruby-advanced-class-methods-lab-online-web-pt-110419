93a4c09c5a02e269f25785f22467c5d580ede6ac
Angelica  Thompson <i.thompson89@outlook.com>
2019-12-06 01:15:40 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,7 @@
 class Song
   attr_accessor :name, :artist_name
   @@all = []
-
+create = Song.new
   def self.all
     @@all
   end
@@ -9,5 +9,8 @@ class Song
   def save
     self.class.all << self
   end
-
+  
+   def create
+     song.create 
+   end   
 end
