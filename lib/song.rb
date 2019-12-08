06fd0727cbc4f7bceb3e9645772c40d129017b8c
Angelica  Thompson <i.thompson89@outlook.com>
2019-12-08 23:39:19 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -31,9 +31,9 @@ class Song
   end   
   
   def self.find_or_create_by_name(song_name)
-     result = self.find_by_name(song_name)
-     if result 
-       result
+    # result =  self.find_by_name(song_name)
+     if self.find_by_name(song_name)
+       self.find_by_name(song_name)
      else 
        self.create_by_name(name)
     end 
