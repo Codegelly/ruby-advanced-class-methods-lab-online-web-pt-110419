4e61ce66a3bd6c1c2bc4d7735ebbd3cb61570a9f
Angelica  Thompson <i.thompson89@outlook.com>
2019-12-09 00:32:55 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -46,7 +46,7 @@ class Song
    song_arry[1]=song_arry[1].chomp(".mp3")
    song=Song.new 
    song.name = song_arry[1]
-   song.artist_name = song_arry[0]
+   song.artist_name=song_arry[0]
    song
   end  
   
